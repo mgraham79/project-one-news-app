84cab9db641f6a22a50c0b388a54e8d3ca4d1714
Morri1eu <morri1eu@gmail.com>
2018-07-06 08:28:31 -0700

    fixed politifacts to work with the trending searches

--- assets/javascript/trending.js
+++ assets/javascript/trending.js
@@ -14,22 +14,22 @@ $("#run-search").on("click", function (event) {
     // grab user input
     var searchTerms = $("#search-term").val().trim();
 
-    if (arrayOfTerms.includes(searchTerms)){
-       console.log('here')
+    if (arrayOfTerms.includes(searchTerms)) {
+        console.log('here')
     }
-    else{
-    database.ref("/trending").push({ term: searchTerms, dateAdded: firebase.database.ServerValue.TIMESTAMP });
+    else {
+        database.ref("/trending").push({ term: searchTerms, dateAdded: firebase.database.ServerValue.TIMESTAMP });
     }// value = searchTerms;
     // console.log(value);
     // alert("Added to firebase");
 });
 
-var arrayOfTerms= []
+var arrayOfTerms = []
 database.ref("/trending").orderByChild("dateAdded").limitToLast(5).on("child_added", function (snapshot) {
     console.log(snapshot.val());
 
-arrayOfTerms.push(snapshot.val().term) 
-console.log(arrayOfTerms)
+    arrayOfTerms.push(snapshot.val().term)
+    console.log(arrayOfTerms)
 
 
     // append recent searches to the page
@@ -45,12 +45,14 @@ $(document).on("click", ".list-inline-item", function (e) {
     $(".recentPolitifact").empty()
     $("#Left").empty()
     $("#Right").empty()
-    var searchTerm = $(this).text();
-    
+    searchTerm2 = $(this).text();
+
+
+
     var buildURL = function () {
         queryURL = "https://newsapi.org/v2/everything?sources=the-new-york-times&";
         var queryParams = {
-            q: searchTerm,
+            q: searchTerm2,
             pageSize: 5,
             from: $("#start-year").val(),
             to: $("#end-year").val(),
@@ -63,181 +65,202 @@ $(document).on("click", ".list-inline-item", function (e) {
         return queryURL + $.param(queryParams);
     }
 
-        //URL query function for Bretbart News
-        var buildURL2 = function () {
-            queryURL2 = "https://newsapi.org/v2/everything?sources=breitbart-news&";
-            var queryParams = {
-                q: searchTerm,
-                pageSize: 5,
-                from: $("#start-year").val(),
-                to: $("#end-year").val(),
-                apikey: "5573b7a235654d248bf3d502bd3417e6"
-            }
-            console.log(queryURL2 + $.param(queryParams))
-            return queryURL2 + $.param(queryParams)
+    //URL query function for Bretbart News
+    var buildURL2 = function () {
+        queryURL2 = "https://newsapi.org/v2/everything?sources=breitbart-news&";
+        var queryParams = {
+            q: searchTerm2,
+            pageSize: 5,
+            from: $("#start-year").val(),
+            to: $("#end-year").val(),
+            apikey: "5573b7a235654d248bf3d502bd3417e6"
         }
-
-        var database = firebase.database()
-
-
-        // Request for information from New York Times API
-
-        $.ajax({
-            url: buildURL(),
-            method: "GET"
-        }).then(function (response) {
-
-            var results = response.articles
-            console.log(results)
-            for (var i = 0; i < results.length; i++) {
-                // After response from API build card for each news article
-                var resultDisplay = $("<div>")
-                resultDisplay.attr("class", "card ")
-                var resultImage = $("<img>")
-
-                resultDisplay.attr("class", "card-img-top")
-                resultImage.attr("src", results[i].urlToImage)
-                var resultTitle = $("<h5>")
-                resultTitle.text(results[i].title)
-                var resultDescription = $("<p>")
-                resultDescription.text(results[i].description)
-                var resultLink = $("<a>")
-                resultLink.attr("href", results[i].url)
-                resultLink.text("Read more")
-                var resultAuthor = $("<p>")
-                resultAuthor.text(results[i].author)
-                var resultDate = $("<p>")
-                resultDate.text(results[i].publishedAt)
-                resultLink.attr("target", "_blank")
-
-
-                // Creating Div for subject options
-
-                var subjectDiv = $("<div>");
-
-                var subjectSelect = $("<select>");
-                subjectSelect.attr("class", "subject-slug");
-                subjectSelect.attr("id", "subject");
-
-                subjectDiv.attr("class", "form-group");
-                subjectDiv.append("<label for='end-year'>Select Most Relevant Subject</label>");
-                subjectDiv.append(subjectSelect);
-
-                // Getting the options
-                selectSubject();
-
-                // Recommendation Checkmark
-
-                var checkLabel = $("<label>");
-                checkLabel.attr("class", "check-container");
-                checkLabel.text("Would you recommend this article?");
-                var checkInput = $("<input>");
-
-                checkInput.attr("class", "checkbox");
-                checkInput.attr("type", "checkbox");
-                checkInput.attr("value", results[i].url);
-                checkInput.attr("id", "checkboxId_" + i);
-                var checkSpan = $("<span>");
-                checkSpan.attr("class", "checkmark");
-                checkLabel.append(checkInput);
-                checkLabel.append(checkSpan);
-
-                // Append the results data to the resultsDisplay
-                resultDisplay.append(resultTitle)
-                resultDisplay.append(resultAuthor)
-                resultDisplay.append(resultDate)
-                resultDisplay.append(resultImage)
-                resultDisplay.append(resultDescription)
-                resultDisplay.append(resultLink)
-                resultDisplay.append(subjectDiv)
-                resultDisplay.append(checkLabel)
-                $("#Left").append(resultDisplay)
-            };
-        });
-
-
-
-    
-
-
-
-// Request for impformation from Breitbart News API
-
-$.ajax({
-    url: buildURL2(),
-    method: "GET"
-}).then(function (response) {
-
-    var results = response.articles
-    console.log(results)
-    for (var i = 0; i < results.length; i++) {
-        // After response from API build card for each news article
-        var resultDisplay = $("<div>")
-        resultDisplay.attr("class", "card ")
-        var resultImage = $("<img>")
-
-        resultDisplay.attr("class", "card-img-top")
-        resultImage.attr("src", results[i].urlToImage)
-        var resultTitle = $("<h5>")
-        resultTitle.text(results[i].title)
-        var resultDescription = $("<p>")
-        resultDescription.text(results[i].description)
-        var resultLink = $("<a>")
-        resultLink.attr("href", results[i].url)
-        resultLink.attr("target", "_blank")
-        resultLink.text("Read more")
-        var resultAuthor = $("<p>")
-        resultAuthor.text(results[i].author)
-        var resultDate = $("<p>")
-        resultDate.text(results[i].publishedAt)
-
-
-        // Creating Div for subject options
-
-        var subjectDiv = $("<div>");
-
-        var subjectSelect = $("<select>");
-        subjectSelect.attr("class", "subject-slug");
-        subjectSelect.attr("id", "subject");
-
-        subjectDiv.attr("class", "form-group");
-        subjectDiv.append("<label for='end-year'>Select Most Relevant Subject</label>");
-        subjectDiv.append(subjectSelect);
-
-        // Getting the options
-        selectSubject();
-
-
-        // Recommendation Checkmark
-
-        var checkLabel = $("<label>");
-        checkLabel.attr("class", "check-container");
-        checkLabel.text("Would you recommend this article?");
-        var checkInput = $("<input>");
-        checkInput.attr("class", "checkbox");
-        checkInput.attr("type", "checkbox");
-        checkInput.attr("value", results[i].url);
-        checkInput.attr("id", "checkboxId_" + i);
-        var checkSpan = $("<span>");
-        checkSpan.attr("class", "checkmark");
-        checkLabel.append(checkInput);
-        checkLabel.append(checkSpan);
-
-
-        // Append the results data to the resultsDisplay
-        resultDisplay.append(resultTitle)
-        resultDisplay.append(resultAuthor)
-        resultDisplay.append(resultDate)
-        resultDisplay.append(resultImage)
-        resultDisplay.append(resultDescription)
-        resultDisplay.append(resultLink)
-        resultDisplay.append(subjectDiv)
-        resultDisplay.append(checkLabel)
-        $("#Right").append(resultDisplay)
+        console.log(queryURL2 + $.param(queryParams))
+        return queryURL2 + $.param(queryParams)
     }
-})
+
+    var database = firebase.database()
+
+
+    // Request for information from New York Times API
+
+    $.ajax({
+        url: buildURL(),
+        method: "GET"
+    }).then(function (response) {
+
+        var results = response.articles
+        console.log(results)
+        for (var i = 0; i < results.length; i++) {
+            // After response from API build card for each news article
+            var resultDisplay = $("<div>")
+            resultDisplay.attr("class", "card ")
+            var resultImage = $("<img>")
+
+            resultDisplay.attr("class", "card-img-top")
+            resultImage.attr("src", results[i].urlToImage)
+            var resultTitle = $("<h5>")
+            resultTitle.text(results[i].title)
+            var resultDescription = $("<p>")
+            resultDescription.text(results[i].description)
+            var resultLink = $("<a>")
+            resultLink.attr("href", results[i].url)
+            resultLink.text("Read more")
+            var resultAuthor = $("<p>")
+            resultAuthor.text(results[i].author)
+            var resultDate = $("<p>")
+            resultDate.text(results[i].publishedAt)
+            resultLink.attr("target", "_blank")
+
+
+            // Creating Div for subject options
+
+            var subjectDiv = $("<div>");
+
+            var subjectSelect = $("<select>");
+            subjectSelect.attr("class", "subject-slug");
+            subjectSelect.attr("id", "subject");
+
+            subjectDiv.attr("class", "form-group");
+            subjectDiv.append("<label for='end-year'>Select Most Relevant Subject</label>");
+            subjectDiv.append(subjectSelect);
+
+            // Getting the options
+            selectSubject();
+
+            // Recommendation Checkmark
+
+            var checkLabel = $("<label>");
+            checkLabel.attr("class", "check-container");
+            checkLabel.text("Would you recommend this article?");
+            var checkInput = $("<input>");
+
+            checkInput.attr("class", "checkbox");
+            checkInput.attr("type", "checkbox");
+            checkInput.attr("value", results[i].url);
+            checkInput.attr("id", "checkboxId_" + i);
+            var checkSpan = $("<span>");
+            checkSpan.attr("class", "checkmark");
+            checkLabel.append(checkInput);
+            checkLabel.append(checkSpan);
+
+            // Append the results data to the resultsDisplay
+            resultDisplay.append(resultTitle)
+            resultDisplay.append(resultAuthor)
+            resultDisplay.append(resultDate)
+            resultDisplay.append(resultImage)
+            resultDisplay.append(resultDescription)
+            resultDisplay.append(resultLink)
+            resultDisplay.append(subjectDiv)
+            resultDisplay.append(checkLabel)
+            $("#Left").append(resultDisplay)
+        };
+    });
 
 
 
+
+
+
+
+    // Request for impformation from Breitbart News API
+
+    $.ajax({
+        url: buildURL2(),
+        method: "GET"
+    }).then(function (response) {
+
+        var results = response.articles
+        console.log(results)
+        for (var i = 0; i < results.length; i++) {
+            // After response from API build card for each news article
+            var resultDisplay = $("<div>")
+            resultDisplay.attr("class", "card ")
+            var resultImage = $("<img>")
+
+            resultDisplay.attr("class", "card-img-top")
+            resultImage.attr("src", results[i].urlToImage)
+            var resultTitle = $("<h5>")
+            resultTitle.text(results[i].title)
+            var resultDescription = $("<p>")
+            resultDescription.text(results[i].description)
+            var resultLink = $("<a>")
+            resultLink.attr("href", results[i].url)
+            resultLink.attr("target", "_blank")
+            resultLink.text("Read more")
+            var resultAuthor = $("<p>")
+            resultAuthor.text(results[i].author)
+            var resultDate = $("<p>")
+            resultDate.text(results[i].publishedAt)
+
+
+            // Creating Div for subject options
+
+            var subjectDiv = $("<div>");
+
+            var subjectSelect = $("<select>");
+            subjectSelect.attr("class", "subject-slug");
+            subjectSelect.attr("id", "subject");
+
+            subjectDiv.attr("class", "form-group");
+            subjectDiv.append("<label for='end-year'>Select Most Relevant Subject</label>");
+            subjectDiv.append(subjectSelect);
+
+            // Getting the options
+            selectSubject();
+
+
+            // Recommendation Checkmark
+
+            var checkLabel = $("<label>");
+            checkLabel.attr("class", "check-container");
+            checkLabel.text("Would you recommend this article?");
+            var checkInput = $("<input>");
+            checkInput.attr("class", "checkbox");
+            checkInput.attr("type", "checkbox");
+            checkInput.attr("value", results[i].url);
+            checkInput.attr("id", "checkboxId_" + i);
+            var checkSpan = $("<span>");
+            checkSpan.attr("class", "checkmark");
+            checkLabel.append(checkInput);
+            checkLabel.append(checkSpan);
+
+
+            // Append the results data to the resultsDisplay
+            resultDisplay.append(resultTitle)
+            resultDisplay.append(resultAuthor)
+            resultDisplay.append(resultDate)
+            resultDisplay.append(resultImage)
+            resultDisplay.append(resultDescription)
+            resultDisplay.append(resultLink)
+            resultDisplay.append(subjectDiv)
+            resultDisplay.append(checkLabel)
+            $("#Right").append(resultDisplay)
+        }
+    })
+
+
+    database.ref('Terms/' + searchTerm2).once('value').then(function (snapshot99) {
+        // console.log(snapshot99.val())
+        
+        var mostRecent = snapshot99.val()
+        console.log(snapshot99.val())
+        if (mostRecent.politifactSpeaker == undefined){
+                $('.recentPolitifact').hide()
+            }
+        else{
+        $('.recentPolitifact').show()
+            $('.recentPolitifact').append('<h5>' + mostRecent.politifactSpeaker + '<h5>')
+            $('.recentPolitifact').append('<p>' + mostRecent.politifactExplanation + '<p>')
+            var polImage = $('<img>')
+            polImage.attr('src', mostRecent.politifactImage)
+            polImage.css('height', '100px')
+            $('.recentPolitifact').append(polImage)
+            $('.recentPolitifact').append('<p>' + mostRecent.politifactText + '<p>')
+        }
+            
+
+
+    })
 
 })
